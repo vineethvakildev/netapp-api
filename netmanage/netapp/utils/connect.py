cb4f599f141479dd618076d8324d6cdf7ffd0f7e
vineethvakil <vineeth.vakil@iproov.com>
2021-12-12 18:19:19 +0000

    improved exception handling and error response

--- netmanage/netapp/utils/connect.py
+++ netmanage/netapp/utils/connect.py
@@ -1,94 +1,90 @@
-from rest_framework.response import Response
-from rest_framework import status
-
 from ncclient.operations.rpc import RPCError
 from ncclient.transport.errors import SessionCloseError, SSHError, SSHUnknownHostError
 from ncclient import manager
 from netmiko import ConnectHandler
-from netmiko.ssh_exception import NetMikoTimeoutException
-from paramiko.ssh_exception import SSHException
+from netmiko.ssh_exception import NetMikoTimeoutException, SSHException
 from ntc_templates.parse import parse_output
 
 from ..models import AllDevices
 from ..serializers import NetmikoSerializer, NetconfSerializer
 
-def handle_error(content):
-    res = {"success": True, "errors": None, "details": None}
-    if validation.is_valid():
-        res["success"] = True
-    else:
-        res["success"] = False
-        res["details"] = "Bad Request Body"
-        res["errors"] = validation.errors
-    return res
-
 
 def get_connection_params(host):
-    result = {"success": True, "errors": None, "details": None, "connection_params": None, "platform": None }
+    result = {"success": True, "errors": None, "details": None}
+    connection_params = None
+    text_fsm_platform = None
     try:
         queryset = AllDevices.objects.get(host__iexact=host)
-        platform = queryset.platform
+        text_fsm_platform = queryset.platform
         device_details = NetmikoSerializer(queryset)
         connection_params = device_details.data
-        result['connection_params'] = connection_params
-        result['platform'] = platform
     except Exception as e:
         result["success"] = False
         result["details"] = "Invalid host"
         result["errors"] = e.__class__.__name__
 
-    return result
+    return result, connection_params, text_fsm_platform
+
 
 def get_nc_connection_params(host):
-    queryset = AllDevices.objects.get(host__iexact=host)
-    device_details = NetconfSerializer(queryset)
-    connection_params = device_details.data
-    connection_params['device_params'] = {'name': queryset.device_params}
-    return connection_params
+    result = {"success": True, "errors": None, "details": None}
+    connection_params = None
+
+    try:
+        queryset = AllDevices.objects.get(host__iexact=host)
+        device_details = NetconfSerializer(queryset)
+        connection_params = device_details.data
+        connection_params['device_params'] = {'name': queryset.device_params}
+    except Exception as e:
+        result["success"] = False
+        result["details"] = "Invalid host"
+        result["errors"] = e.__class__.__name__
+
+    return result, connection_params
 
 
 def nc_connect(connection_params, data):
-    message = ""
+    result = {"success": True, "errors": None, "details": "Configuration applied"}
+
     try:
         with manager.connect(**connection_params) as m:
             nc_reply = m.edit_config(data, target="candidate")
-            nc_reply = m.commit()
-
             if nc_reply.ok:
-                message = "Success"
-                code = status.HTTP_200_OK
-    except RPCError as e:
-        message = f"Error: {e.__class__.__name__} occurred - {e.tag}"
-    except (SessionCloseError, SSHError, SSHUnknownHostError) as e:
-        message = f"Error: {e.__class__.__name__} occurred - {e.tag}"
-    except Exception as e:
-        message = f"Error: {e.__class__.__name__} occurred - {e.tag}"
-    finally:
-        response = Response(data={
-            "message": message
-        }, status=code)
+                nc_reply = m.commit()
+                if not nc_reply.ok:
+                    result["success"] = False
+                    result["details"] = "Configuration commit failed"
+            else:
+                result["success"] = False
+                result["details"] = "Configuration apply failed"
 
-        return response
+    except (RPCError, SessionCloseError, SSHError, SSHUnknownHostError) as e:
+        result["success"] = False
+        result["details"] = e.tag
+        result["errors"] = e.__class__.__name__
+    except Exception as e:
+        result["success"] = False
+        result["details"] = "Unknown error"
+        result["errors"] = e.__class__.__name__
+
+    return result
 
 
 def cli_connect(connection_params, command, text_fsm_platform):
-    interface_parsed = {}
-    message = ""
+    result = {"success": True, "errors": None, "details": None}
+
     try:
         net_connect = ConnectHandler(**connection_params)
         output = net_connect.send_command(command)
         interface_parsed = parse_output(platform=text_fsm_platform, command=command, data=output)
-        message = "Success"
-        code = status.HTTP_200_OK
+        result["details"] = interface_parsed
     except (NetMikoTimeoutException, SSHException) as e:
-        message = f"Error: {e.__class__.__name__} occurred - {e.tag}"
-        code = status.HTTP_503_SERVICE_UNAVAILABLE
+        result["success"] = False
+        result["details"] = e.tag
+        result["errors"] = e.__class__.__name__
     except Exception as e:
-        message = f"Error: {e.__class__.__name__} occurred - {e.tag}"
-        code = status.HTTP_500_INTERNAL_SERVER_ERROR
-    finally:
-        response = Response(data={
-            "message": message,
-            "interfaces": interface_parsed
-        }, status=code)
-        return response
+        result["success"] = False
+        result["details"] = "Unknown error"
+        result["errors"] = e.__class__.__name__
+
+    return result
--- netmanage/netapp/views.py
+++ netmanage/netapp/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework import status
-from django.views import View
 from rest_framework.views import APIView
+
 from . import validators
-from jinja2 import Template
 from .utils.xml_templates import loopback_ipv4_config, loopback_ipv6_config, loopback_delete_config
 from .utils.connect import nc_connect, get_nc_connection_params, get_connection_params, cli_connect
+
+from jinja2 import Template
 import json
 
 
@@ -21,6 +20,10 @@ class ConfigureDeviceView(APIView):
         if not validation["success"]:
             return Response(data=validation, status=status.HTTP_400_BAD_REQUEST)
 
+        result, connection_params = get_nc_connection_params(content['host'])
+        if not result["success"]:
+            return Response(data=result, status=status.HTTP_400_BAD_REQUEST)
+
         if content['protocol'] == 'ipv4':
             tm = Template(loopback_ipv4_config)
         else:
@@ -29,12 +32,12 @@ class ConfigureDeviceView(APIView):
         payload = tm.render(loopbacks=content['loopbacks'])
 
         if content['dryrun'] in ['True', 1]:
-            return JsonResponse(data=payload, safe=False)
+            return Response(data=payload, status=status.HTTP_200_OK)
 
-        connection_params = get_nc_connection_params(content['host'])
-
-        response = nc_connect(connection_params, payload)
-        return response
+        result = nc_connect(connection_params, payload)
+        if not result["success"]:
+            return Response(data=result, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(data=result, status=status.HTTP_201_CREATED)
 
     def delete(self, request):
 
@@ -44,27 +47,34 @@ class ConfigureDeviceView(APIView):
         if not validation["success"]:
             return Response(data=validation, status=status.HTTP_400_BAD_REQUEST)
 
+        result, connection_params = get_nc_connection_params(content['host'])
+        if not result["success"]:
+            return Response(data=result, status=status.HTTP_400_BAD_REQUEST)
+
         tm = Template(loopback_delete_config)
         payload = tm.render(loopbacks=content['loopbacks'])
 
         if content['dryrun'] in ['True', 1]:
-            return JsonResponse(data=payload, safe=False)
+            return Response(data=payload, status=status.HTTP_200_OK)
 
-        connection_params = get_nc_connection_params(content['host'])
-
-        response = nc_connect(connection_params, payload)
-        return response
+        result = nc_connect(connection_params, payload)
+        if not result["success"]:
+            return Response(data=result, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(data=result, status=status.HTTP_200_OK)
 
 
 class ListInterfacesView(APIView):
 
     def get(self, request):
-
         host = self.request.GET['host']
 
-        connection_params, text_fsm_platform = get_connection_params(host)
+        result, connection_params, text_fsm_platform = get_connection_params(host)
+        if not result["success"]:
+            return Response(data=result, status=status.HTTP_400_BAD_REQUEST)
 
         command = "show ip interface brief"
-        response = cli_connect(connection_params, command, text_fsm_platform)
+        result = cli_connect(connection_params, command, text_fsm_platform)
 
-        return response
+        if not result["success"]:
+            return Response(data=result, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(data=result, status=status.HTTP_200_OK)
